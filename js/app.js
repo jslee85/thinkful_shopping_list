e83e659e6cab1bda5d4b7c481ef29bbf58c6be67
Jonathan Lee <Jonathan@macmini-wifi.omghax.biz>
2014-08-06 07:41:39 -0700

    first attempt at jquery

--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
 $(document).ready(function() {
-	$()
+
+
+	$('input').keyup(function() {
+    	var newitem = $(this).val();
+    	$('span').text(newitem);
+ 	})
+ 	
+ 	.keyup();
+
+	$('.input').keypress(function (e) {
+		if (e.which == 13) {
+    		$('form#login').submit();
+    		return false;    //<---- Add this line
+  		}
+	});
+
+ 	$('.newitem').on('click', function() {
+
+ 	});
+
+	$('.licompleteditem').mouseenter(function() {
+		$('.deletebutton').hide();
+		$('.deletebuttonred').show();
+	})
+	
+	.mouseleave(function() {
+		$('.deletebuttonred').hide();
+		$('.deletebutton').show();
+	});
+
+	$('.deletebuttonred').on('click', function() {
+		$(this).closest('.licompleteditem').remove();
+	});
 });
+
+
+
+    getItem();
