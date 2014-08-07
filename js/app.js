7ffa55759e812291ddd8f92a734bba6cccf779d2
Jonathan Lee <Jonathan@macmini-wifi.omghax.biz>
2014-08-06 21:40:12 -0700

    clean up javascript and css

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
 	/*From stack exchange. Input value with enter key...I think?*/
 	$('#addtext').keypress(function (e) {
-		if (e.which == 13) {
-    		var newitem = $(this).val();
-    		$('.yourshoppinglist').prepend('<li class="shoppinglistitem">'+newitem+'</li>');
+		if (e.keyCode === 13) {
+    		var newitem = '<li class="shoppinglistitem">'
+    		var newitem += $(this).val();
+    		var newitem += '<span>'    		
+    		var newitem += '<i class="deletebutton"></i>'
+    		var newitem += '</li>'
+       		$('.yourshoppinglist').prepend(+newitem+);
     		return false;    //<---- Add this line
   		}
 	});
@@ -13,25 +17,8 @@ $(document).ready(function() {
 		$(this).toggleClass('licompleteditem');
 	});
 
-	/*First attempt--did not work!*/
-	/*$('.yourshoppinglist li').click(function() {
-		$('.yourshoppinglist li').removeClass('shoppinglistitem').addClass('licompleteditem');
-		$(this).toggleClass('shoppinglistitem licompleteditem')
-	});*/
-
-	/*Hover state for delete button*/
-	$('.deletebutton').mouseenter(function() {
-		$(this).hide();
-		$('.deletebuttonred').show();
-	});
-	
-	$('.deletebuttonred').mouseleave(function() {
-		$(this).hide();
-		$('.deletebutton').show();
-	});
-
 	/*Allows user to delete items*/
-	$('.deletebuttonred').on('click', function() {
+	$('.licompleteditem .deletebutton').on('click', function() {
 		$(this).closest('.licompleteditem').remove();
 	});
 });
