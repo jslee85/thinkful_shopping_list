7758da0906da82bfefc61d6db8c6048b4b39b23f
Jonathan Lee <Jonathan@macmini-wifi.omghax.biz>
2014-08-09 17:59:21 -0700

    change button to icon and add pseudo selectors

--- js/app.js
+++ js/app.js
@@ -1,24 +1,29 @@
 $(document).ready(function() {
-	/*From stack exchange. Input value with enter key...I think?*/
+	/*From stack exchange. Input value with enter key...I think?
 	$('#addtext').keypress(function (e) {
 		if (e.keyCode === 13) {
-    		var newitem = '<li class="shoppinglistitem">'
-    		var newitem += $(this).val();
-    		var newitem += '<span>'    		
-    		var newitem += '<i class="deletebutton"></i>'
-    		var newitem += '</li>'
-       		$('.yourshoppinglist').prepend(+newitem+);
-    		return false;    //<---- Add this line
-  		}
-	});
+			var newitem = '<li class="shoppinglistitem"><span>';
+			var newitem += '"$(this).val();"';
+			var newitem += '</span>';
+			var newitem += '<i class="deletebutton"></i>';
+			var newitem += '</li>';
+			$('.yourshoppinglist').prepend(newitem);
+			$('#addtext').val('');
+			return false;
+		}
+	});*/
 
 	/*Toggle between complete and incomplete*/
-	$('.shoppinglistitem').on('click', function() {
+	$('.yourshoppinglist').on('click', '.shoppinglistitem', function() {
 		$(this).toggleClass('licompleteditem');
 	});
 
+	$('.yourcompletedlist').on('click', '.licompleteditem', function() {
+		$(this).toggleClass('shoppinglistitem');
+	});
+
 	/*Allows user to delete items*/
-	$('.licompleteditem .deletebutton').on('click', function() {
+	$('.licompleteditem').on('click', '.deletebutton', function() {
 		$(this).closest('.licompleteditem').remove();
 	});
 });
